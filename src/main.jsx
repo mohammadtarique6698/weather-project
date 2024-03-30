dc9d02a53bf96262cdcd76e7db197a76bc23ae77
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2024-03-30 18:47:42 +0530

    small changes.

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/weather",
-        element: (props) => <Navigation />,
+        element: <Navigation />,
       },
     ],
   },
