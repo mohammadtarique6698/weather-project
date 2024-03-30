7b275367f0d33cc918d3d3e63be4659db71499c1
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2024-03-30 17:56:23 +0530

    small changes.

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -5,11 +5,9 @@ import Weather from "./Weather.jsx";
 import Footer from "./Footer.jsx";
 
 const Navigation = () => {
-  // Check if localStorage item exists and is not null
   const userData = JSON.parse(localStorage.getItem("userData")) || {};
   const { given_name, family_name } = userData;
 
-  // Concatenate given_name and family_name if available
   const fullName =
     given_name && family_name ? `${given_name} ${family_name}` : "";
 
